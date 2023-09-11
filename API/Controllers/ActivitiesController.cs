371e37072e13107670f3f4e9c0e6e9d85314b04f
NebulaXI <syyordanova@gmail.com>
2023-09-11 07:58:31 +0300

    End of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,27 +1,46 @@
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using static Application.Activities.Details;
 
 namespace API.Controllers
 {
     public class ActivitiesController:BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-            
-        }
-
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities(){
-            return await _context.Activities.ToListAsync();
+        public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken ct)
+        {
+            return await Mediator.Send(new List.Query(),ct);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id){
-            return await _context.Activities.FindAsync(id);
+        public async Task<ActionResult<Activity>> GetActivity(Guid id,CancellationToken ct){
+            return await Mediator.Send(new Details.Query{Id=id},ct);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity,CancellationToken ct)
+        {
+            await Mediator.Send(new Create.Command{Activity=activity},ct);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id,Activity activity,CancellationToken ct)
+        {
+            activity.Id=id;
+            await Mediator.Send(new Edit.Command{Activity=activity},ct);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id,CancellationToken ct)
+        {
+            await Mediator.Send(new Delete.Command{Id=id},ct);
+            return Ok();
         }
     }
 }
